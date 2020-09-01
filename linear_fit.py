e2f8a9cccabed86aeaf9cc6d2cec234ed77a6e56
hesong <hesonghesong@126.com>
2020-09-01 23:20:16 +0800

    add GNU GPL

--- linear_fit.py
+++ linear_fit.py
@@ -1,3 +1,19 @@
+# Simulation of least-squares linear fitting
+# Copyright (C) <2020>  <coldworm>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import matplotlib.pyplot as plot
 import random
 
@@ -38,4 +54,4 @@ if __name__ == '__main__':
     plot.plot(x, y)
     plot.plot(x, y_linear)
     plot.show()
-    # input("input any key to continue.")
+    # input("press any key to continue.")
